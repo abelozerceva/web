e67a876cfd78ce9f536b52129b32eb22d70e84e7
Anastasia <belozerceva.a@yandex.ru>
2020-01-06 20:54:06 +0300

    Начало реализации веб-приложения с курса по js на stepik.org

---
+++ js_stepik/swapi-ui-js/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    var includes = $('[data-include]');
+    //document.querySelector("[data-include]").innerText;
+    jQuery.each(includes, function(){
+        var file = 'components/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
+
+$('.menu-btn').on('click',function () {
+    $('.menu-wrapper').slideToggle('slow');
+ });
