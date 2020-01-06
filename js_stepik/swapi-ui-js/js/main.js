5869be1fd7a4e66ed03eb985ae3cc3729fe17ad8
Anastasia <belozerceva.a@yandex.ru>
2020-01-06 22:56:30 +0300

    Сделана анимация выпада менюшки, исправлено оформление этой менюшки

--- js_stepik/swapi-ui-js/js/main.js
+++ js_stepik/swapi-ui-js/js/main.js
@@ -10,3 +10,18 @@ $(function(){
 $('.menu-btn').on('click',function () {
     $('.menu-wrapper').slideToggle('slow');
  });
+
+let editPopup = document.querySelector('.edit-popup');
+let editPopupSwitcher = false;
+
+$('#edit').on('click', function (){
+    this.classList.remove('pulse');
+    editPopupSwitcher = !editPopupSwitcher;
+    if (editPopupSwitcher) {
+        TweenMax.to(editPopup, 1, {x:'2%', display:'block', width:'60%', height:'65hv', ease: Back.easeOut});
+    }
+    else {
+        TweenMax.to(editPopup, 1, {x:'-200%', display:'block', width:'60%', height:'0hv', ease: Power3.easeOut});
+    }
+});
+
