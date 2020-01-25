05f7986a8f6d41fcb5c935fd025c37e47c0da00b
Anastasia <belozerceva.a@yandex.ru>
2020-01-26 01:10:53 +0300

    Реализована анимация табов (git, email), а также смена контента при нведении на таб

---
+++ js_stepik/swapi-ui-js/js/tabs.js
@@ -0,0 +1,50 @@
+function Tab(tab, index) {
+    return `<li class="tab-link_tab" id=${index}>
+                <span></span>${tab.name}
+            </li>`
+}
+
+function TabsList(tabs) {
+    return `<ul>
+                ${tabs.map((el, i) => Tab(el, i)).join('')}
+            </ul>`
+}
+
+function TabContent(tab) {
+    return `<div class="tabs_content_container" style="display:block">
+                <a href=${tab.contentLink} target="_blank">${tab.contentLinkText}</a>
+                <p>${tab.contentText}</p>
+            </div>`
+}
+
+let tabs = [
+    {
+        name: 'GitHub',
+        contentLink: 'https://github.com/belozercevaa',
+        contentLinkText: '@github/belozerceva',
+        contentText: 'Здесь можете посмотреть мои проекты'
+    },
+    {
+        name: 'Email',
+        contentLink: 'mailto:belozerceva.a@yandex.ru',
+        contentLinkText: 'belozerceva.a@yandex.ru',
+        contentText: 'Написать мне'
+    },
+    {
+        name: 'VK',
+        contentLink: 'https://vk.com/anastasya29',
+        contentLinkText: 'Белозерцева Анастасия',
+        contentText: 'Связаться со мной через социальную сеть'
+    }
+];
+
+$('.tabs_container')[0].innerHTML = TabsList(tabs);
+
+$('.tab-link_tab').hover(function () {
+    $(this)
+        .addClass('active_tab-link')
+        .siblings()
+        .removeClass('active_tab-link')
+    $('.tab-content_wrapper')[0].innerHTML = TabContent(tabs[+this.id]);
+});
+
