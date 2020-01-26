d797451ed4e0286f9c5bc2d379e7deb5fd658af9
Anastasia <belozerceva.a@yandex.ru>
2020-01-26 16:25:32 +0300

    Реализовано отображение футера на обеих страницах, выпалает сообщение при наведении на футер

--- js_stepik/swapi-ui-js/js/data-page.js
+++ js_stepik/swapi-ui-js/js/data-page.js
@@ -2,6 +2,15 @@ import {theme} from './theme.js'
 
 theme.update();
 
-window.addEventListener('storage',function (event) {
+window.addEventListener('storage',(event) => {
     theme.update();
  });
+
+ $(() => {
+    var includes = $('[data-include]');
+    //document.querySelector("[data-include]").innerText;
+    jQuery.each(includes, function(){
+        var file = 'components/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
