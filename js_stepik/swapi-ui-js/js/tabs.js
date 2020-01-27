e53d98f221f6e670bc1cad228eb9e14d6895c9a1
Anastasia <belozerceva.a@yandex.ru>
2020-01-27 14:12:17 +0300

    добавлены комментарии к функциям

--- js_stepik/swapi-ui-js/js/tabs.js
+++ js_stepik/swapi-ui-js/js/tabs.js
@@ -1,22 +1,23 @@
+// генерация html для одного таба
 function Tab(tab, index) {
     return `<li class="tab-link_tab" id=${index}>
                 <span></span>${tab.name}
             </li>`
 }
-
+// объединение табов в список
 function TabsList(tabs) {
     return `<ul>
                 ${tabs.map((el, i) => Tab(el, i)).join('')}
             </ul>`
 }
-
+// html для отображаемого контента для каждого таба
 function TabContent(tab) {
     return `<div class="tabs_content_container" style="display:block">
                 <a href=${tab.contentLink} target="_blank">${tab.contentLinkText}</a>
                 <p>${tab.contentText}</p>
             </div>`
 }
-
+// содержание табов и контента к ним
 let tabs = [
     {
         name: 'GitHub',
@@ -37,9 +38,9 @@ let tabs = [
         contentText: 'Связаться со мной через социальную сеть'
     }
 ];
-
+// добавление списка табов внутрь класса tabs_container в index.html
 $('.tabs_container')[0].innerHTML = TabsList(tabs);
-
+// смена контента к табу при наведении на таб
 $('.tab-link_tab').hover(function () {
     $(this)
         .addClass('active_tab-link')
